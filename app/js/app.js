2552711751b6c813dc6f8c1928dea3f59f4940eb
Nikki Thean <nikki.thean@gmail.com>
2014-02-10 11:17:23 -0500

    Initial commit

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,19 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var chineseApp = angular.module('chineseApp', ['ngRoute', 'chineseControllers']);
+
+chineseApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    // when('/index', {
+    //   templateUrl: 'partials/index.html',
+    //   controller: 'ChineseListCtrl'
+    // }).
+    when('/search/:meaning', {
+      templateUrl: 'partials/characters.html',
+      controller: 'ChineseCharacterCtrl'
+    }).
+    otherwise({
+      redirectTo: '/index'
+    });
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,16 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var chineseControllers = angular.module('chineseControllers', []);
 
-  }])
-  .controller('MyCtrl2', [function() {
+chineseControllers.controller('ChineseListCtrl', ['$scope', '$http', function($scope, $http) {
+  // $http.get('').success(function(data) {
+  //   $scope.characters = data;
+  // });
+}]);
 
-  }]);
+chineseControllers.controller('ChineseCharacterCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  $http.get('http://ccdb.hemiola.com/characters/definition/' + $routeParams.meaning + '/?fields=string,kMandarin,kDefinition').success(function(data) {
+    $scope.characters = data;
+  })
+}])
